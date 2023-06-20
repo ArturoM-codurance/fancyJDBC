87ce4d8ee11e1c67ed3d337f7229236ef553e7d8
Moises Rodriguez <moisesrj97@gmail.com>
2023-06-20 09:45:09 +0200

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    testImplementation("org.mockito:mockito-core:5.2.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.2.0")
+    implementation("org.postgresql:postgresql:42.6.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
